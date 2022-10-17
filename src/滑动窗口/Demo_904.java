818ce8dcec5ddba02a17de36eed69b8e6ea663b5
Alias <Alias@163.com>
2022-10-17 17:34:09 +0800

    904-滑动窗口(优化)

--- src/滑动窗口/Demo_904.java
+++ src/滑动窗口/Demo_904.java
@@ -12,12 +12,20 @@ public class Demo_904 {
     public static void main(String[] args) {
         int[] fruits1 = {0, 1, 2, 2};
         int[] fruits2 = {3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4};
-        System.out.println(totalFruit(fruits1));
-        System.out.println(totalFruit(fruits2));
+        System.out.println(totalFruit1(fruits1));
+        System.out.println(totalFruit1(fruits2));
+
+        System.out.println(totalFruit2(fruits1));
+        System.out.println(totalFruit2(fruits2));
     }
 
-    // 滑动窗口
-    public static int totalFruit(int[] fruits) {// 水果类型
+    /**
+     * 双指针(两层遍历)<br>
+     * 执行用时：6 ms, 在所有 Java 提交中击败了85.00%的用户<br>
+     * 内存消耗：50.9 MB, 在所有 Java 提交中击败了80.00%的用户<br>
+     * 2022年04月02日  22:10:58
+     */
+    public static int totalFruit1(int[] fruits) {// 水果类型
         int res = 0, len = fruits.length;
 
         // 0 <= fruits[i] < fruits.length => 使用数字哈希统计窗口中果树数量
@@ -44,4 +52,39 @@ public class Demo_904 {
 
         return res;
     }
+
+    /**滑动窗口(一次遍历)<br>
+     执行用时：5 ms, 在所有 Java 提交中击败了97.47%的用户<br>
+     内存消耗：50.6 MB, 在所有 Java 提交中击败了45.09%的用户<br>
+     2022年10月17日  17:23:37*/
+    public static int totalFruit2(int[] fruits) {// 水果类型
+        int res = 0, len = fruits.length;
+
+        // 最多只能够凑成两种类型
+        if (len <= 2) return len;
+
+        for (int preVar = fruits[0], newOrin = 0,// 下一个待匹配类型, 下次窗口起始位置
+             left = 0, right = 1; right < len; ++right) {
+
+            if (fruits[right] != fruits[right - 1]) {// 说明遇到不同中类型水果
+
+                // 说明不是两种水果中的一种 => 1, 2, 1 (更新起点)
+                if (fruits[right] != preVar) left = newOrin;
+
+                // 更新下一个待匹配水果类型(两种中的一种)
+                // 这次 fruits[right] 匹配 1, fruits[right - 1] 匹配 2
+                // 那么下次 fruits[right] 为 2 的时候, 才能够不需要进行更新窗口
+                preVar = fruits[right - 1];
+
+                // 每次进行更新, 因为是不同类型了
+                // 如果下次匹配的时候不是这两种类型的一种, 那么序列中不能夹杂其它类型
+                newOrin = right;
+            }
+
+            res = Math.max(res, right - left + 1);
+        }
+
+        return res;
+    }
+
 }
