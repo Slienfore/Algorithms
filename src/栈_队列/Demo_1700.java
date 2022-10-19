e81f5dd81aac728aacb9793e30f74ee12324b2c6
Alias <Alias@163.com>
2022-10-19 09:55:31 +0800

    1700-规律

--- src/栈_队列/Demo_1700.java
+++ src/栈_队列/Demo_1700.java
@@ -15,7 +15,31 @@ public class Demo_1700 {
         // int[] sandwiches = {0, 1, 0, 1};
         int[] students = {1, 1, 1, 0, 0, 1};
         int[] sandwiches = {1, 0, 0, 0, 1, 1};
-        System.out.println(countStudents(students, sandwiches));
+        System.out.println(countStudents1(students, sandwiches));
+        System.out.println(countStudents2(students, sandwiches));
+    }
+
+    /**
+     * 规律<br>
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户<br>
+     * 内存消耗：39.6 MB, 在所有 Java 提交中击败了38.06%的用户<br>
+     * 2022年10月19日  09:52:48
+     */
+    public static int countStudents2(int[] students, int[] sandwiches) {
+        int[] cnt = new int[2];
+
+        // 统计学生对两种三明治的需求量
+        for (int val : students) ++cnt[val];
+
+        for (int val : sandwiches) {// 遍历栈顶三明治
+            // 只要找到一个三明治所有学生都不喜欢(即此时 学生们不对该三明治有需求了 )
+            // 那么该三明治无论学生怎么排队, 都不会有人吃掉
+            if (cnt[val] > 0) --cnt[val];
+            else
+                break;
+        }
+
+        return cnt[0] + cnt[1];
     }
 
     /**
@@ -24,7 +48,7 @@ public class Demo_1700 {
      * 内存消耗：39.8 MB, 在所有 Java 提交中击败了13.75%的用户<br>
      * 2022年10月19日  09:25:12
      */
-    public static int countStudents(int[] students, int[] sandwiches) {
+    public static int countStudents1(int[] students, int[] sandwiches) {
         Queue<Integer> queue = new LinkedList<>();
 
         // 模拟学生队列
